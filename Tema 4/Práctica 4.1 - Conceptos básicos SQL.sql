6bca276b776b2881eb124cfd6a346a6d0fcbe755
gersancabo <gersancabo@gmail.com>
2021-01-10 22:57:29 +0000

    Práctica 4.1 - Conceptos básicos SQL

---
+++ Tema 4/Práctica 4.1 - Conceptos básicos SQL.sql
@@ -0,0 +1,23 @@
+CREATE DATABASE Jardineria;
+
+USE Jardineria;
+
+-- Aquí se deberia de añadir el script
+
+SHOW DATABASES;
+
+SHOW FULL TABLES FROM Jardineria;
+
+SHOW COLUMNS FROM Jardineria.Clientes;
+
+SHOW COLUMNS FROM Jardineria.Oficinas;
+SHOW COLUMNS FROM Jardineria.Empleados;
+SHOW COLUMNS FROM Jardineria.GamasProductos;
+SHOW COLUMNS FROM Jardineria.Pedidos;
+SHOW COLUMNS FROM Jardineria.Productos;
+SHOW COLUMNS FROM Jardineria.DetallePedidos;
+SHOW COLUMNS FROM Jardineria.Pagos;
+
+SELECT * FROM Clientes;
+
+SELECT NombreCliente, Telefono FROM Clientes;
